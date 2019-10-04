77aeefe7ddf2c6fc029dc0044392cfc0bb453c1d
Michael <michaelcrandall33@gmail.com>
2019-10-04 11:51:33 -0700

    Added servo and limitswitch

--- src/main/java/frc/team6059/robot/RobotMap/IO.java
+++ src/main/java/frc/team6059/robot/RobotMap/IO.java
@@ -3,20 +3,24 @@ package frc.team6059.robot.RobotMap;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Servo;
 
 public class IO {
-    Compressor _compressor = new Compressor(3);
-    DoubleSolenoid _rightPistonSlide = new DoubleSolenoid(3, 0, 1);
-    DoubleSolenoid _leftPistonSlide = new DoubleSolenoid(3, 2, 3);
-    WPI_TalonSRX _frontRightDrive = new WPI_TalonSRX(4);
-    WPI_TalonSRX _frontLeftDrive = new WPI_TalonSRX(5);
-    WPI_VictorSPX _backRightDrive = new WPI_VictorSPX(6);
-    WPI_VictorSPX _backLeftDrive = new WPI_VictorSPX(7);
-    WPI_VictorSPX _elevatorMotor = new WPI_VictorSPX(8);
-    WPI_VictorSPX _ballIntakeMotor = new WPI_VictorSPX(9);
-    WPI_VictorSPX _rightFlywheel = new WPI_VictorSPX(10);
-    WPI_VictorSPX _leftFlywheel = new WPI_VictorSPX(11);
+    DigitalInput _elevatorHome = new DigitalInput(0); //Elevator
+    Servo _hatchHook = new Servo(0); //HatchPanel
+    Compressor _compressor = new Compressor(3); //Ball
+    DoubleSolenoid _rightPistonSlide = new DoubleSolenoid(3, 0, 1); //Ball
+    DoubleSolenoid _leftPistonSlide = new DoubleSolenoid(3, 2, 3); //Ball
+    WPI_TalonSRX _frontRightDrive = new WPI_TalonSRX(4); //DriveTrain
+    WPI_TalonSRX _frontLeftDrive = new WPI_TalonSRX(5); //DriveTrain
+    WPI_VictorSPX _backRightDrive = new WPI_VictorSPX(6); //DriveTrain
+    WPI_VictorSPX _backLeftDrive = new WPI_VictorSPX(7); //DriveTrain
+    WPI_VictorSPX _elevatorMotor = new WPI_VictorSPX(8); //Elevator
+    WPI_VictorSPX _ballIntakeMotor = new WPI_VictorSPX(9); //Ball
+    WPI_VictorSPX _rightFlywheel = new WPI_VictorSPX(10); //Ball
+    WPI_VictorSPX _leftFlywheel = new WPI_VictorSPX(11); //Ball
 
     public IO() {
         _frontRightDrive.configFactoryDefault();
