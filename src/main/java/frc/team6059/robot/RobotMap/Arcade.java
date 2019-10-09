a6bce2309fd6cbc660b2c96fb9d76620daad6000
Michael <michaelcrandall33@gmail.com>
2019-10-09 13:49:16 -0700

    formated

--- src/main/java/frc/team6059/robot/RobotMap/Arcade.java
+++ src/main/java/frc/team6059/robot/RobotMap/Arcade.java
@@ -3,21 +3,21 @@ package frc.team6059.robot.RobotMap;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class Arcade {
-    private static Joystick arcadeStick = new Joystick(1);
+    private static Joystick arcade = new Joystick(1);
 
     public static boolean _elvUp() { //Elevator
-        return arcadeStick.getPOV(0) == 0 || arcadeStick.getPOV(0) == 45 || arcadeStick.getPOV(0) == 315;
+        return arcade.getPOV(0) == 0 || arcade.getPOV(0) == 45 || arcade.getPOV(0) == 315;
     }
 
     public static boolean _elvDown() { //Elevator
-        return arcadeStick.getPOV(0) == 180 || arcadeStick.getPOV(0) == 135 || arcadeStick.getPOV(0) == 225;
+        return arcade.getPOV(0) == 180 || arcade.getPOV(0) == 135 || arcade.getPOV(0) == 225;
     }
 
-    public static boolean _ballFeederExtend() { return arcadeStick.getRawButton(3); } //Ball
+    public static boolean _ballFeederExtend() { return arcade.getRawButton(3); } //Ball
 
-    public static boolean _ballIntake() { return arcadeStick.getRawButton(2); } //Ball
+    public static boolean _ballIntake() { return arcade.getRawButton(2); } //Ball
 
-    public static boolean _ballOuttake() { return arcadeStick.getRawButton(4); } //Ball
+    public static boolean _ballOuttake() { return arcade.getRawButton(4); } //Ball
 
-    public static boolean _hatchHook() { return arcadeStick.getRawButton(6); } //Hatch Hook
+    public static boolean _hatchHook() { return arcade.getRawButton(6); } //Hatch Hook
 }
--- src/main/java/frc/team6059/robot/RobotMap/IO.java
+++ src/main/java/frc/team6059/robot/RobotMap/IO.java
@@ -2,25 +2,23 @@ package frc.team6059.robot.RobotMap;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.*;
 
 public class IO {
-    DigitalInput _elevatorBottom = new DigitalInput(0); //Elevator
-    Servo _hatchHook = new Servo(0); //HatchPanel
-    Compressor _compressor = new Compressor(3); //Ball
-    DoubleSolenoid _rightPistonSlide = new DoubleSolenoid(3, 0, 1); //Ball
-    DoubleSolenoid _leftPistonSlide = new DoubleSolenoid(3, 2, 3); //Ball
-    WPI_TalonSRX _frontRightDrive = new WPI_TalonSRX(4); //DriveTrain
-    WPI_TalonSRX _frontLeftDrive = new WPI_TalonSRX(5); //DriveTrain
-    WPI_VictorSPX _backRightDrive = new WPI_VictorSPX(6); //DriveTrain
-    WPI_VictorSPX _backLeftDrive = new WPI_VictorSPX(7); //DriveTrain
-    WPI_VictorSPX _elevatorMotor = new WPI_VictorSPX(8); //Elevator
-    WPI_VictorSPX _ballIntakeMotor = new WPI_VictorSPX(9); //Ball
-    WPI_VictorSPX _rightFlywheel = new WPI_VictorSPX(10); //Ball
-    WPI_VictorSPX _leftFlywheel = new WPI_VictorSPX(11); //Ball
+    public static Ultrasonic     _distance         = new Ultrasonic(1, 1);
+    public static DigitalInput   _elevatorBottom   = new DigitalInput(0); //Elevator
+    public static Servo          _hatchHook        = new Servo(0); //HatchPanel
+    public static Compressor     _compressor       = new Compressor(3); //Ball
+    public static DoubleSolenoid _rightPistonSlide = new DoubleSolenoid(3, 0, 1); //Ball
+    public static DoubleSolenoid _leftPistonSlide  = new DoubleSolenoid(3, 2, 3); //Ball
+    public static WPI_TalonSRX   _frontRightDrive  = new WPI_TalonSRX(4); //DriveTrain
+    public static WPI_TalonSRX   _frontLeftDrive   = new WPI_TalonSRX(5); //DriveTrain
+    public static WPI_VictorSPX  _backRightDrive   = new WPI_VictorSPX(6); //DriveTrain
+    public static WPI_VictorSPX  _backLeftDrive    = new WPI_VictorSPX(7); //DriveTrain
+    public static WPI_VictorSPX  _elevatorMotor    = new WPI_VictorSPX(8); //Elevator
+    public static WPI_VictorSPX  _ballIntakeMotor  = new WPI_VictorSPX(9); //Ball
+    public static WPI_VictorSPX  _rightFlywheel    = new WPI_VictorSPX(10); //Ball
+    public static WPI_VictorSPX  _leftFlywheel     = new WPI_VictorSPX(11); //Ball
 
     public IO() {
         _frontRightDrive.configFactoryDefault();
