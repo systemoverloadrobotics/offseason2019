5568fec57dde71f8df72619a1f2a2dc2f97dc78f
Michael <michaelcrandall33@gmail.com>
2019-10-11 20:21:52 -0700

    test1

--- src/main/java/frc/team6059/robot/RobotMap/Arcade.java
+++ src/main/java/frc/team6059/robot/RobotMap/Arcade.java
@@ -15,6 +15,8 @@ public class Arcade {
 
     public static boolean _ballFeederExtend() { return arcade.getRawButton(3); } //Ball
 
+    public static boolean _test() { return arcade.getRawButton(10); } 
+
     public static boolean _ballIntake() { return arcade.getRawButton(2); } //Ball
 
     public static boolean _ballOuttake() { return arcade.getRawButton(4); } //Ball
