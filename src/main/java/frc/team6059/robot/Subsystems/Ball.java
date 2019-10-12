d922754f8e4d0ed82ed730ee8fcd94e488096f23
Tori Moore <52933665+VMoore20@users.noreply.github.com>
2019-10-11 20:07:52 -0700

    Added compression and reset methods

--- src/main/java/frc/team6059/robot/Subsystems/Ball.java
+++ src/main/java/frc/team6059/robot/Subsystems/Ball.java
@@ -11,16 +11,24 @@ public class Ball {
     }
 
     public void onTickUpdate() {
+        if(Aracade._compressor()) IO._compressor.Start();
+        else IO._compressor.Stop();
         if (Arcade._ballFeederExtend()) this.feederExtend();
+        else this.feederRetract();
         if (Arcade._ballIntake() ^ Arcade._ballOuttake()) {
             if (Arcade._ballIntake()) this.ballIntake();
             else this.ballOuttake();
         }
+        else this.wheelStop();
     }
 
     private void feederExtend(){
         IO._rightPistonSlide.set(DoubleSolenoid.Value.kForward);
         IO._leftPistonSlide.set(DoubleSolenoid.Value.kForward);
+    }
+    private void feederRetract(){
+        IO._rightPistonSlide.set(DoubleSolenoid.Value.kReverse);
+        IO._leftPistonSlide.set(DoubleSolenoid.Value.kReverse);
     }
     private void ballOuttake(){
         IO._ballIntakeMotor.set(ControlMode.PercentOutput,1);
@@ -31,5 +39,10 @@ public class Ball {
         IO._ballIntakeMotor.set(ControlMode.PercentOutput, -1);
         IO._rightFlywheel.set(ControlMode.PercentOutput, 1);
         IO._leftFlywheel.set(ControlMode.PercentOutput, -1);
+    }
+    private void wheelStop(){
+        IO._ballIntakeMotor.set(ControlMode.PercentOutput, 0);
+        IO._rightFlywheel.set(ControlMode.PercentOutput, 0);
+        IO._leftFlywheel.set(ControlMode.PercentOutput, 0);
     }
 }
