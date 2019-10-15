fa92e8b1ce49cc88acc8b9acce8ce3320354129d
yoshiyahoo <josiahtroup@gmail.com>
2019-10-15 09:18:20 -0700

    I did almost everything in the drive train

--- src/main/java/frc/team6059/robot/Subsystems/DriveTrain.java
+++ src/main/java/frc/team6059/robot/Subsystems/DriveTrain.java
@@ -1,11 +1,30 @@
 package frc.team6059.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.team6059.robot.RobotMap.*;
 
+
+
 public class DriveTrain {
+    // Adjustable Parameters
+    static double SpeedMultiplier = 0.4;    // Max Speed
+    static double RotationMultiplier = 0.4;  // Max Rotation.
+    // End of Adjustable Parameters
+
+    // Other Constants
+    static double SpeedReductionConstant = 0.5;  // Cut speed in 1/2 speedReduction is enabled.
+    static double Kp = -0.1f;  // Proportional control constant.
+    static double min_command = 0.05f;   // minimum rotation.
+    // End of Other Constants.
+
+
     DifferentialDrive _RobotDrive;
+
+
 
     public DriveTrain() {
         _RobotDrive = new DifferentialDrive(IO._frontLeftDrive, IO._frontRightDrive);
@@ -21,7 +40,45 @@ public class DriveTrain {
     }
 
     public void onTickUpdate() {
-        _RobotDrive.arcadeDrive(Flightstick._driveForward(), Flightstick._driveRotation());
+        if(Flightstick._autoAlign()) {
+            autoAlign();
+        }
+        else{
+            double sp = Flightstick._driveForward() * SpeedMultiplier;
+            double rt = Flightstick._driveRotation() * RotationMultiplier;
+            _RobotDrive.arcadeDrive(speedDecrease(sp), rt);
+        }
 
     }
+
+    public double speedDecrease(double x) {
+        if(Flightstick._speedReduction()){
+            return x * 0.5;
+        }
+        else{
+            return x;
+        }
+    }
+
+    public double autoAlign() {
+
+        double offset = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+
+        double heading_error = -offset  / 29.8; // LL2:  offset will be a number between -29.8 and 29.8 -- offset will  range from -1 to 1
+        double steering_adjust = Kp * offset;
+        if (offset > 0)
+        {
+            steering_adjust = Kp*heading_error - min_command;
+        }
+        else if (offset < 0)
+        {
+            steering_adjust = Kp*heading_error + min_command;
+        }
+        _RobotDrive.arcadeDrive(Flightstick._driveForward() * SpeedMultiplier, steering_adjust);
+
+
+        return 0;
+    }
+
+
 }
