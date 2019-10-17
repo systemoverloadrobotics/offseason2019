7173e6747540decb2114e6f14893c6277b81e295
Michael <michaelcrandall33@gmail.com>
2019-10-16 19:01:53 -0700

    added hatch from outside file

--- src/main/java/frc/team6059/robot/Subsystems/HatchPanel.java
+++ src/main/java/frc/team6059/robot/Subsystems/HatchPanel.java
@@ -1,14 +1,32 @@
 package frc.team6059.robot.Subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
 import frc.team6059.robot.RobotMap.*;
 
 public class HatchPanel {
 
+    private Servo mServo;
+
+    /**
+     * class constructor
+     */
     public HatchPanel() {
 
+        mServo = IO._hatchHook;
     }
 
+    /**
+     * checks button status
+     */
     public void onTickUpdate() {
 
+        boolean button = Arcade._hatchHook();
+
+        if (button) {
+            mServo.setAngle(90);
+        }
+        else {
+            mServo.setAngle(0);
+        }
     }
 }
